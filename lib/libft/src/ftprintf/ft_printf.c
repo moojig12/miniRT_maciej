aea9f278f50ccae8b574ac68158892ca52b2783e
tesla33io <artemstavropolov@gmail.com>
2024-11-17 16:09:37 +0100

    Fix! (ft_printf): Tip 32 Crash Early

--- lib/libft/src/ftprintf/ft_printf.c
+++ lib/libft/src/ftprintf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mdomnik <mdomnik@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 13:45:26 by astavrop          #+#    #+#             */
-/*   Updated: 2024/10/16 03:15:31 by mdomnik          ###   ########.fr       */
+/*   Updated: 2024/11/17 16:06:53 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ int	ft_dprintf(int fd, const char *format, ...)
 	va_list	arg;
 	int		done;
 
-	if (!format)
-		return (-1);
 	if (*format == '\0')
 		return (0);
 	va_start (arg, format);
